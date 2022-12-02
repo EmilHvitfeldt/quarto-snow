ecbc514acf06a602082d9548758ecfc251a6d36f
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-12-02 11:57:18 -0800

    make work

--- _extensions/snow/snow.lua
+++ _extensions/snow/snow.lua
@@ -1,12 +1,8 @@
 function Meta(m)
-  quarto.doc.addHtmlDependency({
+  quarto.doc.add_html_dependency({
     name = "magic-snowflakes",
     version = "6.0.1",
     scripts = {"assets/snowflakes.min.js"}
   })
-  quarto.doc.addHtmlDependency({
-    name = "snow-init",
-    version = "1.0.0",
-    scripts = {"snow.js"}
-  })
+  quarto.doc.include_file("after-body", "snow-init.html")
 end
